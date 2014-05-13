b74ec759c0ee863cfd3ae92de11a9ee64cc6e6c2
clintonjnelson <clintonjnelson@live.com>
2014-05-12 22:15:55 -0700

    updates asset require & stripe initializer to only use TEST keys

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
-  Rails.configuration.stripe = { :publishable_key => ENV['STRIPE_LIVE_PUBLISH_KEY'],
-                                 :secret_key      => ENV['STRIPE_LIVE_SECRET_KEY'] }
+  Rails.configuration.stripe = { :publishable_key => ENV['STRIPE_TEST_PUBLISH_KEY'],
+                                 :secret_key      => ENV['STRIPE_TEST_SECRET_KEY'] }
 else
   Rails.configuration.stripe = { :publishable_key => ENV['STRIPE_TEST_PUBLISH_KEY'],
                                  :secret_key      => ENV['STRIPE_TEST_SECRET_KEY'] }
